8feda6c5cef2b5979fa88bf3d12aedc387b51fed
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-12-11 16:23:13 +0300

    mobile menu

--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -8,15 +8,24 @@ import { cn } from '@/utilities/ui'
 interface Props {
   data: HeaderType
   className?: string
+  onLinkClick?: () => void
 }
 
-export const HeaderNav: React.FC<Props> = ({ data, className }) => {
+export const HeaderNav: React.FC<Props> = ({ data, className, onLinkClick }) => {
   const navItems = data?.navItems || []
 
   return (
     <nav className={cn('flex gap-3 items-center', className)}>
       {navItems.map(({ link }, i) => {
-        return <CMSLink key={i} {...link} appearance="link" className="text-base" />
+        return (
+          <CMSLink
+            key={i}
+            {...link}
+            appearance="link"
+            className="text-base"
+            onClick={onLinkClick}
+          />
+        )
       })}
     </nav>
   )
