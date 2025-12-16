fe3f438de1611b26e4bc13b06644dbf8093ecdc4
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-12-16 16:13:12 +0300

    footer element order

--- src/Footer/Component.tsx
+++ src/Footer/Component.tsx
@@ -20,13 +20,13 @@ export async function Footer() {
           <Logo />
         </Link>
 
-        <div className="flex flex-col-reverse items-start md:flex-row gap-4 md:items-center">
-          <ThemeSelector />
+        <div className="flex flex-col items-start md:flex-row gap-4 md:items-center">
           <nav className="flex flex-col md:flex-row gap-4">
             {navItems.map(({ link }, i) => {
               return <CMSLink className="text-white" key={i} {...link} />
             })}
           </nav>
+          <ThemeSelector />
         </div>
       </div>
     </footer>
