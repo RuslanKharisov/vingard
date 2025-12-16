48ddd8636eac2f24293495f6142d0497f967298a
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2025-12-16 17:11:15 +0300

    defaultOpenGraph MetaData

--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -3,14 +3,15 @@ import { getServerSideURL } from './getURL'
 
 const defaultOpenGraph: Metadata['openGraph'] = {
   type: 'website',
-  description: 'An open-source website built with Payload and Next.js.',
+  description:
+    'ООО «НПО «Вингард Автоматика» (ИНН 5906152996) — российская ИТ-компания, которая выполняет работы по комплексной автоматизации и цифровизации промышленных производств. Мы разрабатываем и внедряем программное обеспечение для автоматизации и визуализации производственных процессов. Имеем большой опыт работы с программным обеспечением и оборудованием ведущих мировых и отечественных производителей. Работаем на действующих производствах, соблюдая российские и международные стандарты безопасности. …',
   images: [
     {
       url: `${getServerSideURL()}/website-template-OG.webp`,
     },
   ],
-  siteName: 'Payload Website Template',
-  title: 'Payload Website Template',
+  siteName: '«НПО «Вингард Автоматика»',
+  title: 'ООО «НПО «Вингард Автоматика»',
 }
 
 export const mergeOpenGraph = (og?: Metadata['openGraph']): Metadata['openGraph'] => {
